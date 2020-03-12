635866a1aa64f838c7354bcf16115a3a05f683a8
Fabricio Henrique <fabricio.h93@hotmail.com>
2020-03-12 11:20:22 -0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vilarejo-vantagens-back
 Back End do app de vantagens do vilarejo
+
+## Conta do MongoDB
+> ``e-mail:`` dvidadigital@gmail.com
+> ``senha:`` dvida@12
---
+++ index.js
@@ -0,0 +1,4 @@
+const {ApolloServer} = require('apollo-server')
+
+// let server = new ApolloServer()
+// server.listen(4001).then(()=>{console.log('Apollo Server Connected')})
