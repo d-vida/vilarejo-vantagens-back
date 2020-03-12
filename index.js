3aced2778e2a8c0ea91b12561dffb811254aeb93
Fabricio Henrique <fabricio.h93@hotmail.com>
2020-03-12 11:32:22 -0300

    Mongoose connection

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const {ApolloServer} = require('apollo-server')
+const mongoose = require('mongoose')
+
+mongoose.connect(
+  'mongodb+srv://admin:admin@vilarejo-9wp9p.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  {useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:true}
+)
+mongoose.connection.once('open', ()=>{console.log('> MongoDB Atlas Connected')})
 
 // let server = new ApolloServer()
 // server.listen(4001).then(()=>{console.log('Apollo Server Connected')})
