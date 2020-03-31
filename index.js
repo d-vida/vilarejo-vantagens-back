c1f253b7bce6647bb9c614899c53c44e0c4f4a08
Fabricio Henrique <fabricio.h93@hotmail.com>
2020-03-31 10:33:38 -0300

    cors, tratamento de erro e endpoint de update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let {Cupom} = require('./models.js')
 let mongoose = require('mongoose')
 let express = require('express')
 let bodyParser = require('body-parser')
+let cors = require('cors')
 
 mongoose.connect(
 	'mongodb+srv://admin:admin@vilarejo-9wp9p.gcp.mongodb.net/vilarejo?retryWrites=true&w=majority',
@@ -12,15 +13,20 @@ mongoose.connect(
 let app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+app.listen(4000, () => {console.log('listening at 4000')})
 
-app.listen(4000, () => {
-	console.log('listening at 4000')
-})
+/*************
+	ENDPOINTS
+*************/
 
-// endpoints
-app.get('/cupons', async (req, res) => {
-	let result = await Cupom.find().exec()
-	res.send(result)
+app.get('/getCupons', async (req, res) => {
+	try {
+		let result = await Cupom.find().exec()
+		res.send(result)
+	} catch(error) {
+		res.status(500).send(error)
+	}
 })
 
 app.post('/addCupom', async (req,res) => {
@@ -34,6 +40,19 @@ app.post('/addCupom', async (req,res) => {
 })
 
 app.delete('/deleteCupom/:id', async (req,res) => {
-	let result = Cupom.deleteOne({_id: req.params.id}).exec()
-	res.send(result)
+	try{
+		let result = await Cupom.deleteOne({_id: req.params.id}).exec()
+		res.send(result)
+	} catch(error) {
+		res.status(500).send(error)
+	}
+})
+
+app.put('/updateCupom/:id', async (req,res) => {
+	try {
+		let result = await Cupom.updateOne({_id: req.params.id}, req.body)
+		res.send(result)
+	} catch(error) {
+		res.status(500).send(error)
+	}
 })
