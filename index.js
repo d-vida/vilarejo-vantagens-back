c99078102e090c59e13a7bf9f5c3f47c41201f2c
Gustavo Straub <gustavostraubcovers@gmail.com>
2020-09-01 10:51:11 -0300

    heroku changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-app.listen(4000, () => { console.log('listening at 4000') })
+app.listen(process.env.PORT || 4000, () => { console.log('listening at 4000') })
 
 /*************
 	ENDPOINTS
